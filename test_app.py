3fcb655e07e6ac2bf017f91c01e26e27ba64923d
mugdho-ux <mugdho.ux@gmail.com>
2025-09-22 13:58:07 +0600

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
-# Chrome driver path (উইন্ডোজে যেটা ইনস্টল করেছ)
-driver_path = r"D:\mugdho\jenkins-projects\my-website\tests\chromedriver.exe"
+# Auto-download compatible ChromeDriver
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
-
-driver = webdriver.Chrome(executable_path=driver_path)
-
-# ওয়েবসাইট লোকালহোস্টে রান করছে ধরি (http://localhost:8000)
 driver.get("http://localhost:8000/index.html")
 
-# টেস্ট স্টেপস
 greeting = driver.find_element(By.ID, "greeting").text
 assert greeting == "Hello, Jenkins CI/CD!"
 
